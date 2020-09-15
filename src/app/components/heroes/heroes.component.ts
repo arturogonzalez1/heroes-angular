8718c0fcde1065898f786afe26159536cf1ef50d
arturogonzalez1 <eduardoarturo1511@gmail.com>
2020-09-15 18:38:45 -0500

    components and routes added

---
+++ src/app/components/heroes/heroes.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { HeroesService, Heroe } from '../services/heroes.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+})
+export class HeroesComponent implements OnInit {
+  heroes: Heroe[];
+  // tslint:disable-next-line:variable-name
+  constructor(private _heroesService: HeroesService) {
+  }
+  // tslint:disable-next-line:typedef
+  ngOnInit() {
+    this.heroes = this._heroesService.getHeroes();
+  }
+}
